805fd434939aa5dd936d8cbe931f1d1badb004eb
Bill Botonis <billbotonis@gmail.com>
2020-01-14 11:13:45 -0500

    changes in javascript file

--- scripts.js
+++ scripts.js
@@ -3,24 +3,24 @@ document.getElementById("create").addEventListener("click", function(e){
  e.preventDefault(); 
   const seperator = document.getElementById('seperator').value;
  if(seperator=='0'){
-  	var values = document.getElementById("option_values").value.split("\n");
+  	let values = document.getElementById("option_values").value.split("\n");
   }
   else{
-  	var values = document.getElementById("option_values").value.split(seperator);
+  	let values = document.getElementById("option_values").value.split(seperator);
   }
-  var text = '';
+  let text = '';
  
 
-for (var i =0; i < values.length; i++) {
+for (let i =0; i < values.length; i++) {
 	if (i==0){
-		var select = '<select class="'+document.getElementById('select_class').value+'" id="'+document.getElementById('select_id').value+'">';
+		let select = '<select class="'+document.getElementById('select_class').value+'" id="'+document.getElementById('select_id').value+'">';
 		text = text + select +"\n";
 	}
-	var option = '<option value="'+values[i]+'">'+values[i]+'</option>'
+	let option = '<option value="'+values[i]+'">'+values[i]+'</option>'
 	text = text + option + "\n";
 
 	if (i==values.length-1){
-		var select = '</select>';
+		let select = '</select>';
 		text = text + select + "\n";
 	}
 }
